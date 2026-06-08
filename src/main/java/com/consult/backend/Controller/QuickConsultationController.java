1b9f0ac73229a1f5463792859f5601c8c1367f59
Aditya Yadav <144764644+adityayadavms@users.noreply.github.com>
2026-06-09 02:34:40 +0530

    Converting the QuickConsultationController old response to a standardized response.

--- src/main/java/com/consult/backend/Controller/QuickConsultationController.java
+++ src/main/java/com/consult/backend/Controller/QuickConsultationController.java
@@ -1,10 +1,13 @@
 package com.consult.backend.Controller;
 
 
+import com.consult.backend.dto.ApiResponseDto;
 import com.consult.backend.dto.QuickConsultationRequestDto;
 import com.consult.backend.dto.QuickConsultationResponseDto;
 import com.consult.backend.service.QuickConsultationService;
+import com.consult.backend.util.ResponseUtil;
 import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,9 +21,9 @@ public class QuickConsultationController {
     private final QuickConsultationService quickConsultationService;
 
     @PostMapping("/create-order")
-    public QuickConsultationResponseDto createOrder(
+    public ResponseEntity<ApiResponseDto<QuickConsultationResponseDto>> createOrder(
             @RequestBody QuickConsultationRequestDto dto
     ) {
-        return quickConsultationService.createOrder(dto);
+        return ResponseUtil.success(quickConsultationService.createOrder(dto),"Quick Consultation Created");
     }
 }
